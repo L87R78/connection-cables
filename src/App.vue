1a229dd92f01dbf1cd08043e9f558febe698e49d
L87R78 <l87r78@gmail.com>
2023-01-29 15:09:23 +0200

    Added styles in html body

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
   <router-view/>
 </template>
+
+<style lang="scss">
+@import './App.module.scss';
+</style>
+  
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/task-interview/' : '/',
   transpileDependencies: true,
   chainWebpack: config => {
     config.module.rules.delete('eslint');
